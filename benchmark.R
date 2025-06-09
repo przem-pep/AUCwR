754472b9b6524b6d8aa46e3bb4a4fbf4e4000a45
przem-pep <przpeplinski@gmail.com>
2025-06-09 18:26:25 +0200

    dodano nowe funkcje do benchmarku, tymczasowo usunięto niektóre funkcje

--- benchmark.R
+++ benchmark.R
@@ -47,7 +47,7 @@ call_benchmark <- function(pred, target, times = 100) {
     
     AUC_in_tidyverse(-pred, target),
     
-    AUC_cpp(-pred, target),
+    # AUC_cpp(-pred, target),
     
     auc_u(pred),
     
@@ -55,21 +55,21 @@ call_benchmark <- function(pred, target, times = 100) {
     
     scorecard::perf_eva(-pred, target, binomial_metric = "auc", show_plot = FALSE)$binomial_metric$dat$AUC,
     
-    scikitlearn_auc(-pred, target),
+    # scikitlearn_auc(-pred, target),
     
     caTools::colAUC(-pred, target)[1, 1],
     
-    AUC_dt(-pred, target),
+    # AUC_dt(-pred, target),
     
     precrec::auc(precrec::evalmod(precrec::mmdata(scores = -pred, labels = target)))[1, "aucs"],
     
     roc_auc_score(target, -pred),
     
-    fast_auc(-pred, target),
+    # fast_auc(-pred, target),
     
     yardstick::roc_auc_vec(as.factor(target), pred),
     
-    (unname(rcompanion::cliffDelta(pred ~ target, data = as.data.frame(cbind(pred, target)))) + 1) / 2,
+    (rcompanion::cliffDelta(x = pred[target == 0], y = pred[target == 1], digits = 100) + 1) / 2,
     
     ModelMetrics::auc(target, -pred),
     
@@ -79,15 +79,24 @@ call_benchmark <- function(pred, target, times = 100) {
     
     fbroc::perf(fbroc::boot.roc(-pred, as.logical(target)), metric = "auc")$Observed.Performance,
     
+    DescTools::Cstat(-pred, target),
+    
+    effsize::VD.A(pred ~ factor(target))$estimate,
+    
+    rcompanion::vda(x = pred[target == 0], y = pred[target == 1], digits=100),
+    
     times = times
   )
   
   levels(my_benchmark$expr) <- c("ROCR", "pROC", "mltools", "Hmisc", "bigstatsr",
-                               "AUC_in_tidyverse", "AUC_cpp",
-                               "own-U", "AUC-wilcox", "scorecard", "scikit-learn",
-                               "caTools", "own_data.table_AUC", "precrec", "scikit-learn2",
-                               "Rcpp", "yardstick", "rcompanion", "ModelMetrics", "MLmetrics",
-                               "cvAUC", "fbroc")
+                               "own_AUC_tidyverse", # "AUC_cpp",
+                               "own_Mann_Whitney_U", "own_AUC_Wilcox", "scorecard", # "scikit-learn",
+                               "caTools",
+                               # "own_data.table_AUC",
+                               "precrec", "scikit-learn",
+                               # "Rcpp",
+                               "yardstick", "rcompanion::cliffDelta", "ModelMetrics", "MLmetrics",
+                               "cvAUC", "fbroc", "DescTools", "effsize", "rcompanion::vda")
   
   my_benchmark$expr <- reorder(my_benchmark$expr, -my_benchmark$time, FUN = median)
   
