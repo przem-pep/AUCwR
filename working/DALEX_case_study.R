df52c0baaa5bd470e75fe461b3007fb563fd2a63
bkochanski <67311101+bkochanski@users.noreply.github.com>
2025-10-15 08:15:07 +0200

    Update DALEX_case_study.R

--- working/DALEX_case_study.R
+++ working/DALEX_case_study.R
@@ -1,3 +1,18 @@
+#XAi explainable AI
+# Ten wykres nie działa za szybko
+library(DALEX)
+titanic<-archivist::aread("pbiecek/models/27e5c")
+titanic$survived <- ifelse(titanic$survived == "yes", 1, 0)
+titanic_lmr <- rms::lrm(survived == "1" ~ gender + rms::rcs(age) + class + sibsp + parch + fare + embarked, titanic)
+titanic_lmr_exp <- explain(model = titanic_lmr,
+                           data = titanic[, -9],
+                           y = titanic$survived == "1",
+                           label = "Logistic Regression")
+vip_lm <- model_parts(explainer = titanic_lmr_exp, B = 50, N = NULL)
+plot(vip_lm)
+
+
+#Taka funkcja jest "bebechach" pakietu DALEX
 model_performance_auc <- function(predicted, observed) {
   tpr_tmp <- tapply(observed, predicted, sum)
   TPR <- c(0,cumsum(rev(tpr_tmp)))/sum(observed)
@@ -8,13 +23,49 @@ model_performance_auc <- function(predicted, observed) {
   auc
 }
 
+library(Hmisc)
+#somers2(-pred, target)[1]
+
+# Funkcja własna oparta na rangach
+UNN <- function(x,y){
+  y1 = (y==1)
+  n1 = sum(y1) 
+  (mean(rank(x)[y1]) - (n1 + 1)/2)/(length(y) - n1)}
+
+
+# sprawdzenie czy to samo
+pred <- c(1, 1, 2, 3, 4, 4, 5, 5, 6, 7)
+target <- c(1, 1, 1, 1, 0, 1, 0, 0, 1, 0)
+model_performance_auc(-pred, target)
+bigstatsr::AUC(-pred, target)
+Hmisc::somers2(-pred, target)[1]
+UNN(-pred, target)
+
 library(bigstatsr)
 library(tictoc)
 nsim=1e3
+
+#Taka funkcja jest "bebechach" pakietu DALEX
 tic()
 x <- replicate(nsim, model_performance_auc(rnorm(1000), rbinom(1000,1,.2)))
 toc1<-toc()
+#a mogłaby być taka (bigstatsr::AUC)
 tic()
 y <- replicate(nsim, AUC(rnorm(1000), rbinom(1000,1,.2)))
 toc2<-toc()
-(toc1$toc-toc1$tic)/(toc2$toc-toc2$tic)
+# albo taka (Hmisc::somers2)
+tic()
+y <- replicate(nsim, somers2(rnorm(1000), rbinom(1000,1,.2))[1])
+toc3<-toc()
+# albo taka (własna oparta na Hmisc)
+tic()
+y <- replicate(nsim, UNN(rnorm(1000), rbinom(1000,1,.2)))
+toc4<-toc()
+
+print(c(DALEX = (toc1$toc-toc1$tic), 
+        bigstatsr = (toc2$toc-toc2$tic),
+        Hmisc = (toc3$toc-toc3$tic),
+        UNN = (toc4$toc-toc4$tic)))
+
+#przyśpieszenie - krotność (ile razy szybciej)
+(toc1$toc-toc1$tic)/(toc4$toc-toc4$tic)
