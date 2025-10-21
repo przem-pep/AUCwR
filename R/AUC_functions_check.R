fcc509c4d584a4bcf5f1b0ba42835e5c06e6b9a6
bkochanski <67311101+bkochanski@users.noreply.github.com>
2025-10-21 07:54:20 +0200

    Update AUC_functions_check.R

--- R/AUC_functions_check.R
+++ R/AUC_functions_check.R
@@ -8,6 +8,7 @@ caTools::colAUC(-pred, target, alg="Wilcoxon")
 
 fun <- function(p, t){caTools::colAUC(-p, t)[1, 1]}  # sprawdzana funkcja
 #fun <- function(p, t){bigstatsr::AUC(-p, t)}
+#fun <- function(p, t){Hmisc::somers2(-p, t)[1]}
 aucuf <- function(f, p, t){f(p,t)}  # standaryzowana formuła obliczania AUC do checków
 aucuf(fun, pred, target)  # test standaryzowanej foruły
 
@@ -25,10 +26,14 @@ aucuf(fun, c(1, 1, 2, 3, 4, 4, 5, 5, 6, 7), c(1, 1, 1, 1, 1, 1, 0, 0, 0, 0))
 aucuf(fun, c(1, 1, 2, 3, 4, 4, 5, 5, 6, 7), c(2, 2, 1, 1, 0, 1, 0, 0, 1, 0)) 
 # CHECK6 differing lengths; should be error
 aucuf(fun, c(1, 1, 2, 3, 4, 4, 5, 5, 6, 7), c(1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0)) 
-# CHECK7 non-numeric pred; should be error
+# CHECK7 non-numeric pred; should be error (or 0.8541667)
 aucuf(fun, as.character(c(1, 1, 2, 3, 4, 4, 5, 5, 6, 7)), c(1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0)) 
 # CHECK8 logical pred, can be error or can treat TRUE like 1
 aucuf(fun, c(1, 1, 2, 3, 4, 4, 5, 5, 6, 7), c(1, 1, 1, 1, 0, 1, 0, 0, 1, 0)==1) 
+# CHECK9a (NA target) should be 0.8055556 or error 
+aucuf(fun, c(1, 1, 2, 3, 4, 4, 5, 5, 6, 7), c(1, 1, 1, 1, 0, 1, 0, 0, 1, NA)) 
+# CHECK9b (NA pred) should be 0.8055556 or error
+aucuf(fun, c(1, 1, 2, 3, 4, 4, 5, 5, 6, NA), c(1, 1, 1, 1, 0, 1, 0, 0, 1, 0)) 
 
 
 
